9f759ead68b84a26948dddd3f09922b91443afdd
homesweetlove <zlem8952@cu.ac.kr>
2026-04-14 14:48:29 +0900

    14dlf.java 업데이트

--- 14dlf.java
+++ 14dlf.java
@@ -1,39 +1,3 @@
-
-
-
-
-import java.util.*;
-
-public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        HashMap<String, Integer> map = new HashMap<>();
-        System.out.println("** 포인트 관리 프로그램입니다 **");
-        while (true) {
-            System.out.print("이름과 포인트 입력>> ");
-            String name = sc.next();
-            if (name.equals("그만")) break;
-            int point = sc.nextInt();
-            map.put(name, map.getOrDefault(name, 0) + point);
-            List<String> keys = new ArrayList<>(map.keySet());
-            Collections.sort(keys);
-            for (String key : keys) {
-                System.out.print(key + " " + map.get(key));
-                if (!key.equals(keys.get(keys.size() - 1))) {
-                    System.out.print(", ");
-                }
-            }
-            System.out.println();
-        }
-    }
-}
-
-
-
-
-
-
-
 import java.util.*;
 
 abstract class Shape {
@@ -56,29 +20,35 @@ public class Problem4 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         Vector<Shape> v = new Vector<>();
-        System.out.println("그래픽 에디터 beauty를 실행합니다.");
+
         while (true) {
             System.out.print("삽입(1), 삭제(2), 모두 보기(3), 종료(4)>> ");
-            int cmd = sc.nextInt();
+            int cmd = Integer.parseInt(sc.nextLine());
+
             if (cmd == 1) {
                 System.out.print("Line(1), Rect(2), Circle(3)>> ");
-                int type = sc.nextInt();
+                int type = Integer.parseInt(sc.nextLine());
+
                 if (type == 1) v.add(new Line());
                 else if (type == 2) v.add(new Rect());
-                else v.add(new Circle());
-            } else if (cmd == 2) {
+                else if (type == 3) v.add(new Circle());
+            } 
+            else if (cmd == 2) {
                 System.out.print("삭제할 도형의 위치>> ");
-                int idx = sc.nextInt();
-                if (idx < 1 || idx > v.size()) System.out.println("삭제할 수 없습니다.");
-                else v.remove(idx - 1);
-            } else if (cmd == 3) {
+                int idx = Integer.parseInt(sc.nextLine());
+
+                if (idx < 1 || idx > v.size())
+                    System.out.println("삭제할 수 없습니다.");
+                else
+                    v.remove(idx - 1);
+            } 
+            else if (cmd == 3) {
                 for (Shape s : v) s.draw();
-            } else {
-                System.out.println("beauty를 종료합니다.");
+            } 
+            else if (cmd == 4) {
+                System.out.println("프로그램 종료합니다.");
                 break;
             }
         }
     }
 }
-
-
