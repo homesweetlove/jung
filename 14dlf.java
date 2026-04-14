3ef8b14d345dd1cee4ba741bf70da33178938b73
homesweetlove <zlem8952@cu.ac.kr>
2026-04-14 14:53:27 +0900

    14dlf.java 업데이트

--- 14dlf.java
+++ 14dlf.java
@@ -16,32 +16,29 @@ class Circle extends Shape {
     public void draw() { System.out.println("Circle"); }
 }
 
-public class Problem4 {
+public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         Vector<Shape> v = new Vector<>();
-
+        System.out.println("그래픽 에디터 beauty를 실행합니다.");
         while (true) {
+            System.out.print("삽입(1), 삭제(2), 모두 보기(3), 종료(4)>> ");
             int cmd = sc.nextInt();
-
             if (cmd == 1) {
+                System.out.print("Line(1), Rect(2), Circle(3)>> ");
                 int type = sc.nextInt();
                 if (type == 1) v.add(new Line());
                 else if (type == 2) v.add(new Rect());
-                else if (type == 3) v.add(new Circle());
-            } 
-            else if (cmd == 2) {
+                else v.add(new Circle());
+            } else if (cmd == 2) {
+                System.out.print("삭제할 도형의 위치>> ");
                 int idx = sc.nextInt();
-                if (idx < 1 || idx > v.size())
-                    System.out.println("삭제할 수 없습니다.");
-                else
-                    v.remove(idx - 1);
-            } 
-            else if (cmd == 3) {
+                if (idx < 1 || idx > v.size()) System.out.println("삭제할 수 없습니다.");
+                else v.remove(idx - 1);
+            } else if (cmd == 3) {
                 for (Shape s : v) s.draw();
-            } 
-            else if (cmd == 4) {
-                System.out.println("프로그램 종료합니다.");
+            } else {
+                System.out.println("beauty를 종료합니다.");
                 break;
             }
         }
